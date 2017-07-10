1310577eac98e85ee1d92e50cd2ce0b458880a6f
sbrumley <sbrumley@wireless-town.com>
2017-07-10 13:47:15 -0400

    added ''

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ## Set your Environment variables.
 joeAPI="Your API Key" # Joe API Key to allow you're web requests
 joePOLL=15  # Joe Sandbox polling time in Minutes
@@ -6,6 +6,6 @@ joePOLL=15  # Joe Sandbox polling time in Minutes
 sudo echo "export JOE_KEY='${joeAPI}'" | sudo tee -a /etc/environment
 sudo echo "export JOE_POLL='${joePOLL}'" | sudo tee -a /etc/environment
 
-export JOE_KEY=${joeAPI}
-export JOE_POLL=${joePOLL}
+export JOE_KEY='${joeAPI}'
+export JOE_POLL='${joePOLL}'
 
