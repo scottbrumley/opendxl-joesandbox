38b660e91ff6d78f68730144efa00d966a6def25
sbrumley <sbrumley@wireless-town.com>
2017-07-10 12:11:21 -0400

    Added sample Key

---
+++ scripts/env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#!/bin/bash
+
+joeAPI="Your API Key" # Joe API Key to allow you're web requests
+joePOLL=15  # Joe Sandbox polling time in Minutes
+
+sudo echo "export JOE_KEY='${joeAPI}'" | sudo tee -a /etc/environment
+sudo echo "export JOE_POLL='${joePOLL}'" | sudo tee -a /etc/environment
+
