5d7f4c4de344933d927f80bceec162980653ae43
sbrumley <sbrumley@wireless-town.com>
2017-07-10 19:03:49 -0400

    find root dir

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,6 +6,17 @@
 joeAPI="Your API Key" # Joe API Key to allow you're web requests
 joePOLL=15  # Joe Sandbox polling time in Minutes
 
+installSudo(){
+    if ! [ -x "$(command -v sudo)" ]; then
+        echo 'Error: sudo is not installed.' >&2
+        SUDO=""
+        #apt-get install -y sudo
+    else
+        SUDO="sudo "
+    fi
+
+}
+
 if [[ -d "/vagrant" ]]; then
     ROOT_DIR="/vagrant/"
 else
@@ -27,15 +38,25 @@ runProgram(){
     sudo echo "Joe Sandbox Service is Running ..."
 }
 
+function installOpenDXLCLient(){
+    ### Install Open DXL Client
+    echo "Installing Open DXL Client"
+    cd ${ROOT_DIR}
+    ${SUDO}git clone https://github.com/opendxl/opendxl-bootstrap-python.git
+    cd ${ROOT_DIR}opendxl-bootstrap-python
+    ${SUDO}python setup.py install
+}
+
 function installOpenDXLTIEClient {
     ### Install Open DXL TIE Client
     echo "Installing Open DXL TIE Client"
-    cd /vagrant
-    sudo git clone https://github.com/opendxl/opendxl-tie-client-python.git
-    cd /vagrant/opendxl-tie-client-python
+    cd ${ROOT_DIR}
+    ${SUDO}git clone https://github.com/opendxl/opendxl-tie-client-python.git
+    cd ${ROOT_DIR}opendxl-tie-client-python
     sudo python setup.py install
 }
 
+installSudo
 installOpenDXLTIEClient
 installRequests
 runProgram
